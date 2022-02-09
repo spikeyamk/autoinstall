c2e0188f53d5157c1c1c3f2cd78eefd333e42c24
spikeyamk <76853812+spikeyamk@users.noreply.github.com>
2022-02-09 10:04:16 +0100

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# Activating ntp time synchronization
 timedatectl set-ntp true
-# Getiing the partition paths and formatting the partitions
-echo -n "Specify [EFI partition] PATH: "
+
+
+# Enabling parallel downloads in /etc/pacman.conf
+sed -i '/ParallelDownloads = 5/s/^#//g' /etc/pacman.conf
+
+
+# Getiing the partition paths
+echo -n "Specify [EFI partition] PATH: \n"
 read EFIPATH
-printf "%s\n" "$EFIPATH"
-echo -n "Specify [ROOT partition] PATH: "
+printf "%s\n" "$EFIPATH\n"
+echo -n "Specify [ROOT partition] PATH: \n"
 read ROOTPATH
 printf "%s\n" "$ROOTPATH"
 
+
+# Formatting the partitions
 mkfs.fat -F 32 "$EFIPATH"
 mkfs.ext4 "$ROOTPATH"
 
@@ -18,4 +27,7 @@ mkfs.ext4 "$ROOTPATH"
 mount "$ROOTPATH" /mnt
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
+
+
+# Chrooting
 arch-chroot /mnt
