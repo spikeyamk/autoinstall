af778ddd1f3061e7e589bcfec74dba8bce93d3fd
spikeyamk <vojtkoj@protonmail.com>
2022-02-10 02:45:21 +0100

    Fixes

--- postinstall.sh
+++ postinstall.sh
@@ -17,12 +17,12 @@ cp /etc/X11/xinit/xinitrc ~/.xinitrc
 # Installing the suckless utilities
 git clone https://www.github.com/spikeyamk/st-spikeyamk
 cd st-spikeyamk
-sudo make clean install
+makepkg -si
 
 git clone https://www.github.com/spikeyamk/dwm-spikeyamk
 cd dwm-spikeyamk
-sudo make clean install
+makepkg -si
 
 git clone https://www.github.com/spikeyamk/dwmblocks-spikeyamk
 cd dwmblocks-spikeyamk
-sudo make clean install
+makepkg -si
