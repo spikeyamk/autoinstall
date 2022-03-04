a037a3b729e8fd0963225c9591972e994d88d221
spikeyamk <vojtkoj@protonmail.com>
2022-03-04 19:52:50 +0100

    Fixes

--- test/selectlocale.sh
+++ test/selectlocale.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-printf "Now selected the desired locales. We recommend to select at least one locale in UTF-8 enconding\n"
+printf "Now selected the desired locales. We recommend to select at least one locale in UTF-8 enconding.\n"
 printf "[If left blank en_US.UTF-8 is selected by default]\n"
 
-cat locale.gen | grep UTF-8
+grep "UTF" locale.gen | cat -b
+#grep "ISO" locale.gen | cat -b
